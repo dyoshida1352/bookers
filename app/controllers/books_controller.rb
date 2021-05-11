3a0aa6fa40865ca6033c71428bee6bc417fc1650
dyoshida1352 <d.yoshida1352@gmail.com>
2021-05-11 17:27:42 +0000

    [Add]フラッシュメッセージとバリデーション以外完成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,7 +6,7 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to '/books'
+    redirect_to books_path
   end
   def show
     @book = Book.find(params[:id])
@@ -17,14 +17,14 @@ class BooksController < ApplicationController
   def update
     book = Book.find(params[:id])
     book.update(book_params)
-    redirect_to book_path(book.id)
+    redirect_to show_books_path(book.id)
   end
   def destroy
     book = Book.find(params[:id])
     book.destroy
     redirect_to books_path
   end
-  
+
   private
   def book_params
     params.require(:book).permit(:title, :body)
